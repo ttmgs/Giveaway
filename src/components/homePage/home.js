8dd1b2eb01c90427b4afcc33bdc53231cbf1d254
Magdi Tiea <magditiea@gmail.com>
2021-05-30 01:15:51 -0400

    button added to home

--- src/components/homePage/home.js
+++ src/components/homePage/home.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import '../styles/home.css';
+import { Link, useLocation } from 'react-router-dom'
+
 
 const Home = () => {
+  const location = useLocation();
+
   return(
-    <h1>yeooo</h1>
+    <div class="container-fluid">
+      <h2>HOW TO ENTER </h2>
+      <div id="box">
+        <h1>Send a $30 etransfer to fariqjafer@gmail.com with your name in the notes </h1>
+      </div>
+
+<div id="btnBox">
+      <button class="button button5">
+      <Link
+            to="/prizes"
+            className={
+              location.pathname === "/prizes"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+        Prizes</Link></button>
+      </div>
+      </div>
   )
 
 }
--- src/components/prizes/prizes.js
+++ src/components/prizes/prizes.js
@@ -4,10 +4,17 @@ import Scooter from '../images/scooter.jpg'
 
 const Prizes = () => {
   return(
+    <div>
     <div class="container-fluid">
     <img src={Scooter} alt="yo"/>
   </div>
-
+  <div id="des">
+<h2>Gotrax Electric Scooter</h2>
+<li>The adult electric powered scooter features a quiet 250W motor </li>
+  <li>boasting top speed of 15.5 MPH and max load of 220 lbs. </li>
+  <li>  Tackles steep 14° hills with ease, making it a perfect commuting scooter.</li>
+  </div>
+  </div>
   )
 }
 
